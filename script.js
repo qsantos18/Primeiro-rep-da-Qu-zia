855e82b73a6bc17148751c341cc4e995e474b347
qsantos18 <q.almeida@escola.pr.gov.br>
2026-04-24 13:10:55 +0000

    .

--- script.js
+++ script.js
@@ -14,30 +14,30 @@ let card = document.querySelector(".apresentacao");
 botaoMensagem.addEventListener("click", function () {
 
     // quando o botão for clicado, troca o texto do parágrafo pela mensagem desejada
-        mensagem.innerText = "Olá! Seja bem-vindo ao meu mini currículo.";
-        });
+    mensagem.innerText = "Olá! Seja bem-vindo ao meu mini currículo.";
+});
 
-        // cria uma variável para controlar se a cor atual do card foi alterada ou não
-        let corAtual = 0;
+// cria uma variável para controlar se a cor atual do card foi alterada ou não
+let corAtual = 0;
 
-        // adiciona um evento de clique no botão de mudar cor
-        botaoCor.addEventListener("click", function () {
+// adiciona um evento de clique no botão de mudar cor
+botaoCor.addEventListener("click", function () {
 
-            // verifica se o valor atual da variável é 0
-                if (corAtual == 0) {
+    // verifica se o valor atual da variável é 0
+    if (corAtual == 0) {
 
-                        // se for 0, muda a cor de fundo do card para azul claro
-                                card.style.backgroundColor = "#dbeafe";
+        // se for 0, muda a cor de fundo do card para azul claro
+        card.style.backgroundColor = "#dbeafe";
 
-                                        // altera o valor da variável para 1, indicando que a cor foi trocada
-                                                corAtual = 1;
+        // altera o valor da variável para 1, indicando que a cor foi trocada
+        corAtual = 1;
 
-                                                    } else {
+    } else {
 
-                                                            // caso contrário, volta a cor de fundo do card para branco
-                                                                    card.style.backgroundColor = "white";
+        // caso contrário, volta a cor de fundo do card para branco
+        card.style.backgroundColor = "white";
 
-                                                                            // altera o valor da variável para 0, indicando que voltou ao estado inicial
-                                                                                    corAtual = 0;
-                                                                                        }
-                                                                                        });
+        // altera o valor da variável para 0, indicando que voltou ao estado inicial
+        corAtual = 0;
+    }
+});
